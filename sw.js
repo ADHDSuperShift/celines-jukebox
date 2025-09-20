eeb092b83c403f75aa99ed156c05fb8716d5dc0a
Dirk Marais <dirkmarais@Dirks-MacBook-Air.local>
2025-09-20 13:54:47 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'celines-jukebox-v1';
+const CACHE_NAME = 'celines-jukebox-v2';
 const urlsToCache = [
   '/celines-jukebox/',
   '/celines-jukebox/assets/',
@@ -9,6 +9,19 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Clean up old caches on activate
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
   );
 });
 
